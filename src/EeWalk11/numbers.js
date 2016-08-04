3f1597a84eace811ea735366846c0cf17a6e2f47
Eric Walker <eewalk11@gmail.com>
2016-08-04 12:03:18 -0600

    Added functions for manipulating integer strings

--- src/EeWalk11/numbers.js
+++ src/EeWalk11/numbers.js
@@ -18,6 +18,65 @@ EeWalk11.isInteger = function(arg0) {
 
 
 
+EeWalk11.isIntish = function(arg0) {
+	var i = parseInt(arg0);
+	return !Number.isNaN(i) && i == EeWalk11.stripZeros(String(arg0));
+};
+
+
+
+/**
+ * Strip leading zeros from a numeric string. This function will not validate that the string is
+ * actually numeric.
+ * @param {String}  str  The string.
+ * @return {String}  The result. The argument will simply be returned if it is not a string.
+ */
+EeWalk11.stripLeadingZeros = function(str) {
+	if(typeof str === "string") {
+		str = str.replace(/^[0]+/, "");
+		if(str === "" || str.charAt(0) === ".") {
+			str = "0" + str;
+		}
+	}
+	return str;
+}
+
+
+
+/**
+ * Strip trailing zeros after a decimal from a numeric string. This function will not validate that
+ * the string is actually numeric.
+ * @param {String}  str  The string.
+ * @return {String}  The result. The argument will simply be returned if it is not a string.
+ */
+EeWalk11.stringTrailingZeros = function(str) {
+	if(typeof str === "string" && str.indexOf(".") >= 0) {
+		str = str.replace(/[0]+$/, "");
+		if(str.charAt(str.length - 1) === ".") {
+			//Remove a trailing decimal point
+			str = str.substring(0, str.length - 1);
+		}
+		if(str === "") {
+			str = "0";
+		}
+	}
+	return str;
+}
+
+
+
+/**
+ * Strip leading and trailing zeros from a numeric string. This function will not validate that the
+ * string is actually numeric.
+ * @param {String}  str  The string.
+ * @return {String}  The result. The argument will simply be returned if it is not a string.
+ */
+EeWalk11.stripZeros = function(str) {
+	return stripTrailingZeros(stripLeadingZeros(str));
+}
+
+
+
 /**
  * Number.MAX_SAFE_INTEGER. This avoids compatibility issues.
  * @type {Number}
